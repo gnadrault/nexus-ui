cc54a7f8f879234869a56c4c0e82e002a08382a4
gnadrault <gnadro@gmail.com>
2022-04-20 21:46:36 +0200

    Update popup

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,61 +1,45 @@
-(function () {
-  var app_transform = "";
+// Card popup
+export default function popup() {
+  var popup_transform = "";
+  var $body = document.body;
+  var $popupContainer = document.querySelector(".card-popup");
 
-  var $body = document.body,
-    $appContainer = document.querySelector(".c-app-container"),
-    $appContainer_title = $appContainer.querySelector(".c-app-container__title"),
-    $appContainer_body = $appContainer.querySelector(".c-app-container__body"),
-    $apps = document.querySelectorAll(".c-app");
+  var openPopup = function (e) {
+    var popup_bounding_rect = $popupContainer.getBoundingClientRect(),
+      card_bounding_rect = e.target.getBoundingClientRect(),
+      translateX = card_bounding_rect.left + "px",
+      translateY = card_bounding_rect.top + "px",
+      scaleX = card_bounding_rect.width / popup_bounding_rect.width,
+      scaleY = card_bounding_rect.height / popup_bounding_rect.height;
 
-  var getApp = function (e) {
-    var index = getIndex(e.target),
-      $app = $apps[index],
-      app_title = $app.querySelector(".c-app__title").textContent,
-      app_body = $app.querySelector(".c-app__body").innerHTML;
-
-    $appContainer_title.innerHTML = app_title;
-    $appContainer_body.innerHTML = app_body;
-    $appContainer.setAttribute("data-bg-color", e.target.getAttribute("data-bg-color"));
-
-    openApp(e);
+    popup_transform = "translate(" + translateX + "," + translateY + ") scale(" + scaleX + ", " + scaleY + ")";
+    $popupContainer.style.transform = popup_transform;
+    document.body.offsetWidth;
+    $body.classList.add("popup-open");
+    $popupContainer.style.transform = "translate(calc(50vw - 50%), calc(50vh - 50%)) scale(1)";
   };
 
-  var openApp = function (e) {
-    var app_bounding_rect = $appContainer.getBoundingClientRect(),
-      tile_bounding_rect = e.target.getBoundingClientRect();
-    (translateX = tile_bounding_rect.left + "px"),
-      (translateY = tile_bounding_rect.top + "px"),
-      (scaleX = tile_bounding_rect.width / app_bounding_rect.width),
-      (scaleY = tile_bounding_rect.height / app_bounding_rect.height);
-
-    app_transform = "translate3d(" + translateX + "," + translateY + ",0) scale(" + scaleX + ", " + scaleY + ")";
-    $appContainer.style.transform = app_transform;
-    document.body.offsetWidth; // force reflow
-    $body.classList.add("-app-open");
-    $appContainer.style.transform = "translate3d(0,0,0) scale(1)";
+  var closePopup = function (e) {
+    $popupContainer.addEventListener("transitionend", resetPopup, false);
+    $popupContainer.style.transform = popup_transform;
+    $body.classList.add("popup-close");
+    $body.classList.remove("popup-open");
   };
 
-  var closeApp = function (e) {
-    $appContainer.addEventListener("transitionend", resetApp, false);
-    $appContainer.style.transform = app_transform;
-    $body.classList.add("-app-close");
-    $body.classList.remove("-app-open");
-  };
-
-  var resetApp = function (e) {
-    if (e.target === $appContainer) {
-      $body.classList.remove("-app-close");
-      $appContainer.removeAttribute("style");
-      $appContainer.removeEventListener("transitionend", resetApp);
+  var resetPopup = function (e) {
+    if (e.target === $popupContainer) {
+      $body.classList.remove("popup-close");
+      $popupContainer.removeAttribute("style");
+      $popupContainer.removeEventListener("transitionend", resetPopup);
     }
   };
 
   document.addEventListener(
     "click",
     function (e) {
-      var matches = e.target.matches(".c-app__tile");
+      var matches = e.target.matches(".card");
       if (matches) {
-        getApp(e);
+        openPopup(e);
       }
     },
     false
@@ -64,21 +48,21 @@
   document.addEventListener(
     "click",
     function (e) {
-      var matches = e.target.matches(".c-app-container__dismiss");
+      var matches = e.target.matches(".card-popup__dismiss");
       if (matches) {
-        closeApp(e);
+        closePopup(e);
       }
     },
     false
   );
 
-  var getIndex = function (node) {
-    var children = node.parentNode.childNodes;
-    var num = 0;
-    for (var i = 0; i < children.length; i++) {
-      if (children[i] == node) return num;
-      if (children[i].nodeType == 1) num++;
-    }
-    return -1;
-  };
-})();
+  document.addEventListener(
+    "keydown",
+    function (e) {
+      if (e.code === "Escape" && $body.classList.contains("popup-open")) {
+        closePopup(e);
+      }
+    },
+    false
+  );
+}
