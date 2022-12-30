1caedd77f02f937b49a777df8adb2c14b7180dd1
gnadrault <gnadro@gmail.com>
2022-12-30 12:02:40 +0100

    Add declaration type

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import "./sass/style.scss";
+export declare function switchTheme(): void;
+export declare function openDialog(dialog: HTMLDialogElement | null, modal?: boolean): void;
+export declare function closeDialog(dialog: HTMLDialogElement | null): void;
